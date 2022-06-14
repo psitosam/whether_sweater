aa45ef75fbb2aae73f48045c71c2552b5cc0290f
Alex Psitos <apsitos08@gmail.com>
2022-06-13 20:36:03 -0400

    Feat: user request spec passes

---
+++ db/migrate/20220613234326_add_unique_index_to_user.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_index :users, :api_key, unique: true 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_12_225837) do
+ActiveRecord::Schema.define(version: 2022_06_13_234326) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_06_12_225837) do
     t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["api_key"], name: "index_users_on_api_key", unique: true
   end
 
 end
