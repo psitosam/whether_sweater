0f69cee1d810a43c0c7c1d343da7bde2712f8b91
Alex Psitos <apsitos08@gmail.com>
2022-06-14 11:49:45 -0400

    Test: write test for brewery service and pass

--- spec/services/brewery_service_spec.rb
+++ spec/services/brewery_service_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe 'the brewery service' do
     conn = BreweryService.conn
     expect(conn).to be_a(Faraday::Connection)
   end
+
+  it 'returns breweries from a given location' do
+    location = "richmond"
+    response = BreweryService.get_breweries(location)
+
+    expect(response).to be_a Array
+    response.each do |brewery|
+      expect(brewery).to have_key(:id)
+      expect(brewery).to have_key(:name)
+      expect(brewery).to have_key(:brewery_type)
+      expect(brewery).to have_key(:city)
+      expect(brewery).to have_key(:state)
+    end
+  end
 end
