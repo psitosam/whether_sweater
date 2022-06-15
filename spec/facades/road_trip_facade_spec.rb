8aaf0b3b1fd1707e6fb95a05fa92fe68d4190fd2
Alex Psitos <apsitos08@gmail.com>
2022-06-14 20:21:27 -0400

    Test: road trip service spec written and passed

--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -6,12 +6,13 @@ RSpec.describe RoadTripFacade do
     origin = 'washington,dc'
     destination = 'denver,co'
     road_trip = RoadTripFacade.new_road_trip(destination, origin)
+    VCR.use_cassette('new_road_trip') do
 
     expect(road_trip).to be_a Hash
     expect(road_trip).to have_key(:route)
     expect(road_trip[:route]).to have_key(:formattedTime)
     expect(road_trip[:route]).to have_key(:locations)
     expect(road_trip[:route][:locations]).to be_a Array
-
+    end
   end
 end
