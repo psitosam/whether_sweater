a8dd07dbe0177ee10114df7636df46806c86e3e1
Alex <95240894+psitosam@users.noreply.github.com>
2022-08-08 10:59:14 -0400

    Update road_trips_controller.rb

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::RoadTripsController < ApplicationController
     to = params[:destination]
     from = params[:origin]
     api_key = params[:api_key]
-    # require 'pry'; binding.pry
     if User.find_by(api_key: api_key)
       road_trip = RoadTripFacade.route(to, from)
       if road_trip.is_a?(RoadTrip)
